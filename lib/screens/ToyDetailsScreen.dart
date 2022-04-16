adbb439969b60b154ed7d28abcfa48a77d1c3a3d
Justin <Justincravy@csus.edu>
2022-04-16 01:35:32 -0700

    TOYT-39 - fix Create UI for Toy Details Screen Images of toy Text field for description of toy Text field for condition of toy Text field for category of toy Button to contact user if interested in toy

--- lib/screens/ToyDetailsScreen.dart
+++ lib/screens/ToyDetailsScreen.dart
@@ -8,48 +8,51 @@ class ToyDetailsScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     Widget titleSection = Row(
-      children: <Widget>[
-        Expanded(
-          child: new Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              new Text("Toy Name: Nerf Gun", style: new TextStyle(fontSize: 25.0)),
-              new Text("Quality: Excellent", style: new TextStyle(fontSize: 25.0)),
-              new Text("Seller: James Smith", style: new TextStyle(fontSize: 25.0)),
-              new Text("Location: Sacramento, CA", style: new TextStyle(fontSize: 25.0)),
-              new Image.asset('assets/images/greenbutton.png',),
-              new Image.asset('assets/images/redbutton.png',)
-            ],
-          )
-        ),
-      ]
+        children: <Widget>[
+          Expanded(
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  new Text("Toy Name: Nerf Gun", style: new TextStyle(fontSize: 25.0)),
+                  new Text("Quality: Excellent", style: new TextStyle(fontSize: 25.0)),
+                  new Text("Seller: James Smith", style: new TextStyle(fontSize: 25.0)),
+                  new Text("Location: Sacramento, CA", style: new TextStyle(fontSize: 25.0)),
+                  new Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      new Image.asset('assets/images/greenbutton.png',width:250,height:200),
+                      new Image.asset('assets/images/redbutton.png',)
+                    ],
+                  )
+                ],
+              )
+          ),
+        ]
     );
 
     return Scaffold(
       // backgroundColor: const Color(0xffC4DFCB),
-      appBar: AppBar(
-        title: const Text(
-          "Toy Trader",
-          style: TextStyle(
-            // color: Theme.of(context).primaryColor,
-            fontSize: 25,
-            fontWeight: FontWeight.w600,
+        appBar: AppBar(
+          title: const Text(
+            "Toy Trader",
+            style: TextStyle(
+              // color: Theme.of(context).primaryColor,
+              fontSize: 25,
+              fontWeight: FontWeight.w600,
+            ),
           ),
+          // centerTitle: true,
+          // backgroundColor: Colors.white,
         ),
-        // centerTitle: true,
-        // backgroundColor: Colors.white,
-      ),
 
-      body: new ListView(
-        children: <Widget>[
-          new Image.asset(
-            'assets/images/nerfgun1.jpg',
-          ),
-          titleSection,
-        ]
-      )
+        body: new ListView(
+            children: <Widget>[
+              new Image.asset(
+                'assets/images/nerfgun1.jpg',
+              ),
+              titleSection,
+            ]
+        )
     );
   }
 }
-
-
