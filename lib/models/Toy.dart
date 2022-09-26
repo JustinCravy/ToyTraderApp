3cb325632f005f44843ffc3545562300514d43ae
Cotey Hinton <hinton.c32>
2022-09-26 10:52:49 -0700

    TOYT-72

---
+++ lib/models/Toy.dart
@@ -0,0 +1,21 @@
+class Toy {
+
+  final String toyId;
+  final String ownerId;
+  final String name;
+  final String description;
+  final String condition;
+  final String ageRange;
+  final List<String> categories;
+
+  Toy (
+      this.toyId,
+      this.ownerId,
+      this.name,
+      this.description,
+      this.condition,
+      this.ageRange,
+      this.categories
+      );
+  
+}
