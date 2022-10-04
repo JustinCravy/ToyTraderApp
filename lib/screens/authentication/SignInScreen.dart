571ca2be29aee34f7ef186c034b8222155bd6102
DESKTOP-57MAGT4\joseph <joseohac83@gmail.com>
2022-10-03 17:34:17 -0700

    TOYT-75 sign in
    Completed the sign in functionality and added the firebase initialization into the main dart class

--- lib/screens/authentication/SignInScreen.dart
+++ lib/screens/authentication/SignInScreen.dart
@@ -1,28 +1,29 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:toy_trader/screens/authentication/AuthService.dart';
 
 import '../HomeScreen.dart';
 import 'RegistrationScreen.dart';
 
-class SignInScreen extends StatefulWidgetWidget {
+class SignInScreen extends StatefulWidget {
   SignInScreen({Key? key}) : super(key: key);
-  final emailController = TextEditingController();
-  final passwordController = TextEditingController();
 
   @override
-  void dispose(){
-    emailController.dispose();
-    passwordController.dispose();
-  }
+  State<StatefulWidget> createState() => _SignInScreenState();
+}
 
-  
+class _SignInScreenState extends State<SignInScreen> {
+  AuthService authService = AuthService();
+  final _formKey = GlobalKey<FormState>();
+
+  String email = '';
+  String pw = '';
+  String error = '';
 
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
-        // backgroundColor: const Color(0xffC4DFCB),
+      // backgroundColor: const Color(0xffC4DFCB),
         appBar: AppBar(
             title: const Text(
               "Toy Trader",
@@ -49,10 +50,11 @@ class SignInScreen extends StatefulWidgetWidget {
                     style: TextStyle(color: Colors.white),
                   ))
             ]
-            // backgroundColor: Colors.white,
-            ),
+          // backgroundColor: Colors.white,
+        ),
         body: Container(
             padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
+            child: Form(
             child: Column(children: <Widget>[
               Image.asset(
                   'assets/images/logo.png',
@@ -60,27 +62,39 @@ class SignInScreen extends StatefulWidgetWidget {
                   height: 175
               ),
               Form(
+                key: _formKey,
                 child: Column(
                   children: <Widget>[
                     TextFormField(
-                      decoration: InputDecoration(
-                        hintText: 'Email',),
-                      controller: emailController,
-                      validator: (val) => val!.isEmpty ? 'Enter email' : null,),
+                      decoration: InputDecoration(hintText: 'Email',),
+                      validator: (val) => val!.isEmpty ? 'Enter email' : null,
+                      onChanged: (val) {
+                      setState(() => email = val);
+                      },
+                    ),
                     SizedBox(height: 20.0),
                     TextFormField(
                       decoration: InputDecoration(
                         hintText: 'Password',
                       ),
-                      validator: (val) =>
-                          val!.length < 2 ? 'Password must be > 2 chars' : null,
-                      controller: passwordController,
+                      validator: (val) => val!.length < 2 ? 'Password must be > 2 chars' : null,
+                      onChanged: (val) {
+                        setState(() => pw = val);
+                      },
                       obscureText: true,
                     ),
                     SizedBox(height: 20.0),
                     RaisedButton(
                         child: Text('Sign in'),
-                        onPressed: () {
+                        onPressed: () async {
+
+                          if(_formKey.currentState!.validate()){
+                            dynamic result = await authService.signIn(email, pw);
+                            if(result == null){
+                              setState(() => error = 'Couldnt sign in...');
+                            }
+                          }
+
 
                           /*
                           Navigator.push(
@@ -90,12 +104,13 @@ class SignInScreen extends StatefulWidgetWidget {
 
                            */
                         }
-                        ),
+                    ),
                   ],
                 ),
               ),
             ]
             )
+        )
         )
     );
   }
