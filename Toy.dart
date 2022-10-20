779be4d8ffb0a984ad13bacaa9013385f4d73474
BattleT3ch <98723566+BattleT3ch@users.noreply.github.com>
2022-10-19 21:52:10 -0700

    Add files via upload
    Toyadd: Add toy to database, with adjust to other files.

---
+++ Toy.dart
@@ -0,0 +1,24 @@
+class Toy {
+
+   String toyId;
+   String ownerId;
+   String name;
+   String description;
+   String condition;
+   String ageRange;
+   //List<String> categories;
+   String categories;
+   String toyImageURL;
+
+  Toy (
+      this.toyId,
+      this.ownerId,
+      this.name,
+      this.description,
+      this.condition,
+      this.ageRange,
+      this.categories,
+      this.toyImageURL,
+      );
+  
+}
