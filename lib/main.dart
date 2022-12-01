37bfdf205828cf0730fa01ba087e4eeacfff772a
DESKTOP-57MAGT4\joseph <joseohac83@gmail.com>
2022-12-01 14:48:16 -0800

    Added custom color to MAin

--- lib/main.dart
+++ lib/main.dart
@@ -20,16 +20,33 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+
+    //Creates a Custom Color That can be added to the theme of the app,
+    //The first three fields (R,G,B,Opacity);
+    MaterialColor mycolor = MaterialColor(const Color.fromRGBO(162,156,244, 1).value, const <int, Color>{
+      50: Color.fromRGBO(162,156,244, 0.1),
+      100: Color.fromRGBO(162,156,244,  0.2),
+      200: Color.fromRGBO(162,156,244, 0.3),
+      300: Color.fromRGBO(162,156,244,  0.4),
+      400: Color.fromRGBO(162,156,244,  0.5),
+      500: Color.fromRGBO(162,156,244,  0.6),
+      600: Color.fromRGBO(162,156,244,0.7),
+      700: Color.fromRGBO(162,156,244,  0.8),
+      800: Color.fromRGBO(162,156,244, 0.9),
+      900: Color.fromRGBO(162,156,244, 1),
+    },
+    );
     return StreamProvider<ProfileInfo?>.value(
       value: AuthService().user,
       initialData: null,
       child: MaterialApp(
         title: 'Toy Trader',
         debugShowCheckedModeBanner: false,
-        theme: ThemeData(primarySwatch: Colors.deepPurple),
+        theme: ThemeData(primarySwatch: mycolor),
 
         home:  AuthWrapper()
       ),
     );
   }
+
 }
